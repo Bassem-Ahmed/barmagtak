ff1be952f2203180e202ed121ac35e28840abd19
osama <bassemahmade@gmail.com>
2020-06-07 01:37:17 +0200

    modifing my projects

--- js/script.js
+++ js/script.js
@@ -19,4 +19,39 @@ $(function(){
         cursorborderradius:0
     });
     //
+    // start auto slider for testemonials
+    function autoSlider(){
+        $('.t-slider .active').each(function(){
+            if(!$(this).is(':last-child')){
+                $(this).delay(3000).fadeOut(1000, function(){
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+                    autoSlider();
+                })
+            }else{
+                $(this).delay(3000).fadeOut(1000, function(){
+                    $(this).removeClass('active');
+                    $('.t-slider div').eq(0).addClass('active').fadeIn();
+                    autoSlider();
+                })
+            }
+        })
+    };
+    autoSlider();
+    //
+    
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+        } else {
+        $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+        scrollTop: 0
+        }, 1300);
+    });
+    //
 })
